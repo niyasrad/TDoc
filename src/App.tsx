5728bf741b02aab38369e7ebeb6909c7dd4f572b
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-03-06 18:32:33 +0530

    [Niyas] Changed Loading Styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,16 @@ function App() {
   const [token, setToken] = useState<string>('');
   const [sideBarOpen, setSideBarOpen] = useState(false);
 
+  const tips = [
+    "Use the Categories to keep your tasks aligned and organized.",
+    "If you do not clear the tasks before the due, you'll be given alerts.",
+    "The UI Reacts to the alerts, due and priorities of your task.",
+    "If you need motivation, remember you have tasks to do!",
+    "The tasks are filtered through the filter options, near the search!"
+  ]
+
+  const index = Math.floor(Math.random() * (4 - 0)) + 0;
+
   const handleSignOut = () => {
     setAuth(false)
     setToken('')
@@ -64,26 +74,34 @@ function App() {
         {loaded ?
           authenticated ?
             <>
-              <Sidebar open={sideBarOpen} setOpen={() => {setSideBarOpen(!sideBarOpen)}} handleSignOut={handleSignOut}/>
-              <Topbar setOpen={() => {setSideBarOpen(!sideBarOpen)}}/>
+              <Sidebar open={sideBarOpen} setOpen={() => { setSideBarOpen(!sideBarOpen) }} handleSignOut={handleSignOut} />
+              <Topbar setOpen={() => { setSideBarOpen(!sideBarOpen) }} />
               <CardWrapper token={token} handleSignOut={handleSignOut} />
             </>
             :
-            <>
+            <div className='app-real'>
               <div className='app-logo'>
                 <img src={logo} alt='app-logo' />
               </div>
               <Authentication onauth={onAuth} />
-            </>
+            </div>
           :
           <>
-            <div className='app-logo'>
-              <img src={logo} alt='app-logo' />
-            </div>
-            <div className="app-center">
-              <div className="loadingio-spinner-eclipse-hudbooqsn0d"><div className="ldio-c1dvvx8rgm">
-                <div></div>
-              </div></div>
+            <div className="app-real">
+              <div className='app-logo'>
+                <img src={logo} alt='app-logo' />
+              </div>
+              <div className="app-center">
+                <div className="loadingio-spinner-eclipse-hudbooqsn0d"><div className="ldio-c1dvvx8rgm">
+                  <div></div>
+                </div></div>
+                <div className="app-bottom">
+                  <div className="app-center-loading-text">Connecting with the Server</div>
+                  <div className="app-center-tip-container">
+                    <div className="app-center-tip-text">{tips[index]}</div>
+                  </div>
+                </div>
+              </div>
             </div>
           </>
         }
