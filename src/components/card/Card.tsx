0e517ec22631328a5577e5c6bd89c7c08d450451
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-03-20 17:31:48 +0530

    [Niyas] Card Due Glow

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -25,6 +25,9 @@ export default function Card( { _id, title, description, task, due, priority, do
         },
         closed: { opacity: 0, height: "0", y: -20, transition: { duration: 0.2 } }
     };
+
+    
+    const atDue = new Date(new Date().getTime() + 24 * 60 * 60 * 1000).getTime() >= new Date(due).getTime()
     const day = new Date(due).toLocaleString();
     const [expandTask, setExpandTask] = useState(false);
 
@@ -37,7 +40,7 @@ export default function Card( { _id, title, description, task, due, priority, do
         setChange!(!change)
     }
     return (
-        <div className={done ? "card-surface grayglow" :priority === "HIGH" ? "card-surface pinkglow" : "card-surface blueglow"} onClick={() => setExpandTask(!expandTask)} >
+        <div className={done ? "card-surface grayglow" : atDue? "card-surface redglow" : priority === "HIGH" ? "card-surface pinkglow" : "card-surface blueglow"} onClick={() => setExpandTask(!expandTask)} >
             <div className='card-main'>
                 <div className='card-title'>{title}</div>
                 <div className='card-due'>Due on <span className='due-bold'>{day.split(',')[0]}</span></div>
@@ -61,7 +64,7 @@ export default function Card( { _id, title, description, task, due, priority, do
                 
             </div>
             <div className='card-footer'>
-                <div className={done ? "footer-mark-done black" : priority === "LOW" ? "footer-mark-done blue" : "footer-mark-done pink"} 
+                <div className={done ? "footer-mark-done black" : atDue ? "footer-mark-done red" :priority === "LOW" ? "footer-mark-done blue" : "footer-mark-done pink"} 
                 onClick={
                     (e) => {
                         e.preventDefault();
